d8a5d4fe01bdc01a64d6b71b8918720082af8489
LimJaeHyun <guswogus81@gmail.com>
2025-02-07 23:14:14 +0900

    🎉Start : 프로젝트 기본 세팅

---
+++ src/pages/bst/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BstPage() {
+  return <div>밥술팅 메인 페이지입니당</div>;
+}
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <>
+      <div>로그인 페이지에용</div>
+      <div>로그인 페이지에용</div>
+    </>
+  );
+}
