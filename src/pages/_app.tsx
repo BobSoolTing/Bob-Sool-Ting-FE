5d503c49fdb7ca787faaead0ab87c29fb55ef8fb
LimJaeHyun <guswogus81@gmail.com>
2025-02-08 15:17:14 +0900

    🎉Start : 프로젝트 초기 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className='w-full h-screen flex justify-center items-center bg-white'>
+      <div className='w-[412px] h-full'>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
