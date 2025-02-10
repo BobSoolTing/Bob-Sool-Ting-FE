152b82d6b8bf7b5d2cf40e640ab74d0535e3cd85
LimJaeHyun <guswogus81@gmail.com>
2025-02-10 23:27:31 +0900

    🌟 Feat : 버튼 마크업 완료

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-export default function Button({ children }) {
-  return <div>{children}</div>;
+interface ButtonProps {
+  children: ReactNode;
+}
+
+export default function Button({ children }: ButtonProps) {
+  return (
+    <div className='w-[380px] h-[50px] relative overflow-hidden rounded-md bg-[#2f7dff] mx-auto cursor-pointer hover:bg-[#2768FF]'>
+      <p className='w-[210.54px] h-10 absolute left-[85px] top-[5px] text-xl font-semibold text-center text-white content-center'>
+        {children}
+      </p>
+    </div>
+  );
 }
