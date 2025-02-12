c880313b57b113a07e2892095f76985495f9248b
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-02-12 13:27:54 +0900

    📝 Update : 바텀, 카테고리 바 경로 수정

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -24,8 +24,8 @@ export default function BottomBar({ children }: BottomBarProps) {
                         <span className="text-xs font-pretendard font-bold">검색</span>
                     </button>
                 </Link>
-                <Link href={'/bst'}>
-                    <button className={`flex flex-col items-center ${getActiveClass('/bst')}`}>
+                <Link href={'/bst/Bob'}>
+                    <button className={`flex flex-col items-center ${getActiveClass('/bst/Bob')}`}>
                         <BobSoolTingIcon className="fill-current" />
                         <span className="text-xs font-pretendard font-bold">밥술팅</span>
                     </button>
