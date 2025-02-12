5249eaf1239ce3f6b0be6723eb30a2172017aaed
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-02-12 12:38:27 +0900

    📝 Update : 줄바꿈 수정

--- src/pages/my-page/index.tsx
+++ src/pages/my-page/index.tsx
@@ -3,8 +3,9 @@ import React from "react";
 import BottomBar from "@/components/shared/BottomBar";
 
 export default function MyPage() {
-  return <div>마이페이지입니당
-  </div>;
+  return (
+    <div>마이 페이지입니당</div>
+  )
 }
 
 MyPage.getLayout = (page: ReactNode) => {
