36fc0444a95bb18861980467e1b864e83084623b
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-02-20 14:08:09 +0900

    📝 Update: 마진 재설정

--- src/components/shared/AlertItem.tsx
+++ src/components/shared/AlertItem.tsx
@@ -16,19 +16,21 @@ export default function AlertItem({ children }: AlertProps) {
   return (
     <div className='w-[412px] h-[106px] flex items-center justify-between bg-white border-b border-[#d9d9d9] px-4 relative overflow-hidden'>
       {/* 슬라이드 될 영역 */}
-      <div className={`flex items-center justify-between w-full transition-transform duration-300 ${isSlide ? 'transform translate-x-[-72px]' : ''}`}>
+      <div className='w-[412px] h-[106px] flex items-center justify-between bg-white border-b border-[#d9d9d9] px-4'>
         {/* 프로필 아이콘 + 텍스트 컨테이너 */}
         <div className='flex items-center gap-3'>
           <ProfileIcon />
           <div className='flex w-[264px] min-h-[74px] flex-col gap-2'>
             <p className='text-base font-bold text-[#1b1b1b]'>밥약 하실 후배님을 찾습니다.</p>
-            <p className='text-xs font-medium text-[#1b1b1b]'>내용을 작성하지 않았습니다</p>
+            <p className='text-xs font-medium text-[#1b1b1b] flex-grow line-clamp-2'>
+              내용을 작성하지 않았습니다내용을 작성하지 않았습니다내용을 작성하지 않았습니다내용을 작성하지 않았습니다내용을 작성하지 않았습니다
+            </p>
             <p className='text-[10px] font-medium text-[#767676]'>25. 04. 30</p>
           </div>
         </div>
 
         {/* 삭제 영역 열기 */}
-        <div onClick={handleDeleteOpen} className='self-start'>
+        <div onClick={handleDeleteOpen} className='self-start mt-2'>
           <DeleteButtonIcon />
         </div>
       </div>
