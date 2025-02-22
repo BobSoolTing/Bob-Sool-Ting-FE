bbf57cc7f29690d06d72ba011b28ab78a054c130
LimJaeHyun <guswogus81@gmail.com>
2025-02-22 19:55:37 +0900

    📝 Update : SearchCloseIcon 누락 정상화

--- src/assets/icons/SvgIcon.tsx
+++ src/assets/icons/SvgIcon.tsx
@@ -248,3 +248,11 @@ export const AlertIcon = ({ className }: { className?: string }) => {
     </svg>
   );
 };
+
+export const SearchCloseIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg className={className} width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path d='M4 12L12 4M4 4L12 12' stroke='#999999' stroke-width='2' stroke-linecap='round' />
+    </svg>
+  );
+};
