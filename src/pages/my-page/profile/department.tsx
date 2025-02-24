d681488165c7ee57a53145d5db31f28607277213
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-02-25 01:51:09 +0900

    🌟 Feat : 마이 페이지 프로필 설정 폴더 추가

---
+++ src/pages/my-page/profile/department.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function department() {
+  return <div>department</div>;
+}
---
+++ src/pages/my-page/profile/nickname.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function nickname() {
+  return <div>nickname</div>;
+}
