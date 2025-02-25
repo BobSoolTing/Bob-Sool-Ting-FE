f9cf589b26d5b21b7440cd4a4c8b0d90adcef007
LimJaeHyun <guswogus81@gmail.com>
2025-02-25 16:03:45 +0900

    📝 Update : 메인 페이지에서 로그인 페이지로 이동하도록 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,23 @@
-import BottomBar from '@/components/shared/bst/BottomBar';
-import Link from 'next/link';
-import { ReactNode } from 'react';
+import Image from 'next/image';
+import kakaoLogin from '@/assets/images/kakaoLogin.png';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div className='container mx-auto space-y-6'>
-      {/* Search Section */}
-      <div className='text-xl font-semibold text-gray-700'>검색창</div>
-
-      {/* Links Section */}
-      <div className='space-x-4'>
-        <Link href={'/bst/meal/meal'} className='text-blue-500 hover:underline'>
-          밥약
-        </Link>
-        <Link href={'/bst/meal/drink'} className='text-blue-500 hover:underline'>
-          술약
-        </Link>
-        <Link href={'/bst/meal/groupMeet'} className='text-blue-500 hover:underline'>
-          과팅
-        </Link>
-      </div>
-
-      {/* Post Items Section */}
-      <div className='space-y-2'>
-        <div className='p-4 border rounded-lg shadow-md bg-white'>포스트 아이템 1</div>
-        <div className='p-4 border rounded-lg shadow-md bg-white'>포스트 아이템 2</div>
-        <div className='p-4 border rounded-lg shadow-md bg-white'>포스트 아이템 3</div>
-        <div className='p-4 border rounded-lg shadow-md bg-white'>포스트 아이템 4</div>
-        <div className='p-4 border rounded-lg shadow-md bg-white'>포스트 아이템 5</div>
-        <div className='p-4 border rounded-lg shadow-md bg-white'>포스트 아이템 6</div>
+    <div className='w-[412px] h-[100vh] bg-white'>
+      <div className='flex flex-col items-center justify-center h-full gap-8'>
+        <div className='h-[300px] text-2xl font-bold'>로고 변경 예정입니다</div>
+        <Image
+          src={kakaoLogin}
+          alt='카카오 로그인 버튼'
+          className='w-full max-w-[280px] cursor-pointer'
+          onClick={() => {
+            router.push('/login');
+          }}
+        />
       </div>
     </div>
   );
 }
-
-Home.getLayout = (page: ReactNode) => {
-  return <BottomBar>{page}</BottomBar>;
-};
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import Image from 'next/image';
-import kakaoLogin from '@/assets/images/kakaoLogin.png';
 
 export default function LoginPage() {
   return (
     <div className='w-[412px] h-[100vh] bg-white'>
       <div className='flex flex-col items-center justify-center h-full gap-8'>
         <div className='text-2xl font-bold'>로고</div>
-        <Image src={kakaoLogin} alt='카카오 로그인 버튼' className='w-full max-w-[280px] cursor-pointer' />
       </div>
     </div>
   );
