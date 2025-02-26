d19d03b6e8a3ac93e60b8991081788a9cc2360c9
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-02-26 17:32:57 +0900

    📝 Update : 바뀐 파일명으로 글쓰기 버튼 적용

--- src/pages/bst/bob.tsx
+++ src/pages/bst/bob.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ReactNode } from 'react';
 import BottomBar from '@/components/shared/bst/BottomBar';
 import CategoryBar from '@/components/shared/bst/CategoryBar';
-import Writing from '@/components/shared/bst/Writing';
+import Write from '@/components/shared/bst/Write';
 import SearchBar from '@/components/shared/search/SearchBar';
 
 export default function BobPage() {
@@ -16,7 +16,7 @@ BobPage.getLayout = (page: ReactNode) => {
         <SearchBar />
         <BottomBar>
           {page}
-          <Writing />
+          <Write />
         </BottomBar>
       </CategoryBar>
     </>
