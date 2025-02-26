005cf5e3302dcee65b652c1326e4573b86696166
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-02-26 18:53:51 +0900

    📝 Update : 마이-기록 부분 임시 마크업

--- src/pages/my-page/history/comment.tsx
+++ src/pages/my-page/history/comment.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import { ReactNode } from 'react';
+import { useState } from 'react';
 import Header from '@/components/shared/bst/Header';
 import BottomBar from '@/components/shared/bst/BottomBar';
+import { PostItem } from '@/components/shared/post/PostItem';
+
+interface Post {
+  _id: string;
+  title: string;
+  content: string;
+  participants: string[];
+  max_participants: number;
+  location: string;
+  date: string;
+  createdAt: string;
+  like: number;
+  comment: number;
+  view: number;
+}
 
 export default function CommentPage() {
-  return <div>comment</div>;
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  return (
+    <div>
+      {posts.length > 0 ? (
+        posts.map((post) => <PostItem key={post._id} post={post} />)
+      ) : (
+        <div className='flex flex-col items-center justify-center'>
+          <p className='text-xl font-bold text-[#767676]'>댓글을 작성한 모집글이 없습니다.</p>
+          <p className='text-sm text-[#999]'>마음에 드는 모집글을 찾아 댓글을 작성해 보세요!</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 CommentPage.getLayout = (page: ReactNode) => {
--- src/pages/my-page/history/matching.tsx
+++ src/pages/my-page/history/matching.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import { ReactNode } from 'react';
+import { useState } from 'react';
 import Header from '@/components/shared/bst/Header';
 import BottomBar from '@/components/shared/bst/BottomBar';
+import { PostItem } from '@/components/shared/post/PostItem';
+
+interface Post {
+  _id: string;
+  title: string;
+  content: string;
+  participants: string[];
+  max_participants: number;
+  location: string;
+  date: string;
+  createdAt: string;
+  like: number;
+  comment: number;
+  view: number;
+}
 
 export default function MatchingPage() {
-  return <div>matching</div>;
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  return (
+    <div>
+      {posts.length > 0 ? (
+        posts.map((post) => <PostItem key={post._id} post={post} />)
+      ) : (
+        <div className='flex flex-col items-center justify-center'>
+          <p className='text-xl font-bold text-[#767676]'>완료된 매칭이 없습니다.</p>
+          <p className='text-sm text-[#999]'>작성된 모집글을 탐색하고 만남을 신청해 보세요!</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 MatchingPage.getLayout = (page: ReactNode) => {
