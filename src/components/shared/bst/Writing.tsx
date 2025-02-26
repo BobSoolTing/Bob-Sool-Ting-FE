63c2e5c8614eecbecd62bd52c031309f319bf5eb
LimJaeHyun <guswogus81@gmail.com>
2025-02-26 12:22:51 +0900

    📝 Update : 글쓰기 버튼 클릭 시 정확한 주소로 이동

--- src/components/shared/bst/Writing.tsx
+++ src/components/shared/bst/Writing.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 import { WritingIcon } from '@/assets/icons/SvgIcon';
+import { useRouter } from 'next/router';
 
 export default function Writing() {
+  const router = useRouter();
+
   return (
-    <Link href={'/writing'}>
+    <Link href={`${router.asPath}/writing`}>
       <div className='fixed bottom-14 mb-4 ml-[306px]'>
         <div className='w-[90px] h-[35px] relative rounded-[50px] bg-[#2f7dff] hover:bg-[#2768FF] transition-all'>
           <div className='w-full h-full absolute' />
