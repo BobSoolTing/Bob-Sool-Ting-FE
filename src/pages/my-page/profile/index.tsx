de9b8f56d25fffdf4bb14b73da2a01b4c3d9d430
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-02-26 12:47:06 +0900

    📝 Update : 각 페이지 이동 기능 추가

--- src/pages/my-page/profile/index.tsx
+++ src/pages/my-page/profile/index.tsx
@@ -8,23 +8,27 @@ import { DepartmentIcon, MypageIcon } from '@/assets/icons/SvgIcon';
 
 export default function ProfileEditPage() {
   const profileItems = [
-    { icon: MypageIcon, label: '닉네임', value: '신짱구' },
-    { icon: DepartmentIcon, label: '학과', value: '떡잎마을방범학과' },
+    { icon: MypageIcon, label: '닉네임', value: '신짱구', href: '/my-page/profile/nickname' },
+    { icon: DepartmentIcon, label: '학과', value: '떡잎마을방범학과', href: '/my-page/profile/department' },
   ];
 
   return (
     <>
       <div className='flex pt-8 pb-4 border-b-2 border-[#EEEEEE]'>
-        <ProfileItem />
+        <Link href='/my-page/profile/image' className='w-full'>
+          <ProfileItem />
+        </Link>
       </div>
-      <div className='flex flex-col my-4 mx-4 py-2'>
+      <div className='flex flex-col my-4 mx-4'>
         {profileItems.map((item, index) => (
           <div key={index}>
-            <div className='flex items-center h-10 gap-2 px-2'>
-              <item.icon />
-              <div className='text-lg font-medium text-left text-[#1b1b1b]'>{item.label}</div>
-              <div className='text-lg font-medium text-right text-[#999] ml-auto'>{item.value}</div>
-            </div>
+            <Link href={item.href} className='block'>
+              <div className='flex items-center h-10 gap-2 px-2 cursor-pointer'>
+                <item.icon />
+                <div className='text-lg font-medium text-left text-[#1b1b1b]'>{item.label}</div>
+                <div className='text-lg font-medium text-right text-[#999] ml-auto'>{item.value}</div>
+              </div>
+            </Link>
             {index < profileItems.length - 1 && <div className='border-b-2 border-[#F7F7F8] mt-1 mb-1' />}
           </div>
         ))}
