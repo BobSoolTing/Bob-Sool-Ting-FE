a9a8e10e52e40c1cfbaa49e75a4724fdc9493e98
LimJaeHyun <guswogus81@gmail.com>
2025-02-26 16:07:59 +0900

    📝 Update : 버튼 className, isComplete, onClick props 추가

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -2,14 +2,23 @@ import React, { ReactNode } from 'react';
 
 interface ButtonProps {
   children: ReactNode;
+  className?: string;
+  isComplete: boolean;
+  onClick?: () => void;
 }
 
-export default function Button({ children }: ButtonProps) {
+export default function Button({ children, className, isComplete, onClick }: ButtonProps) {
   return (
-    <div className='w-[380px] h-[50px] relative overflow-hidden rounded-md bg-[#2f7dff] mx-auto cursor-pointer hover:bg-[#2768FF]'>
-      <p className='w-[210.54px] h-10 absolute left-[85px] top-[5px] text-xl font-bold text-center text-white content-center'>
-        {children}
-      </p>
+    <div className={className} onClick={onClick}>
+      {isComplete ? (
+        <div className='w-[380px] h-[50px] flex items-center justify-center rounded-md bg-[#2f7dff] mx-auto cursor-pointer hover:bg-[#2768FF]'>
+          <p className='text-xl font-bold text-white'>{children}</p>
+        </div>
+      ) : (
+        <div className='w-[380px] h-[50px] flex items-center justify-center rounded-md bg-[#999999] mx-auto cursor-pointer hover:bg-[#8A8A8A]'>
+          <p className='text-xl font-bold text-white'>{children}</p>
+        </div>
+      )}
     </div>
   );
 }
