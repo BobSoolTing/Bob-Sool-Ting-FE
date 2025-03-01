a5946290165142a69b7695911ef9e29285620171
LimJaeHyun <guswogus81@gmail.com>
2025-03-01 23:44:56 +0900

    🌟 Feat : 인원 바텀 시트 구현

--- src/components/shared/post/BottomSheetPersonnel.tsx
+++ src/components/shared/post/BottomSheetPersonnel.tsx
@@ -1,6 +1,8 @@
-// components/shared/post/BottomSheetPersonnel.tsx
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import BottomSheet from './BottomSheet';
+import { AddIcon, SubtractIcon } from '@/assets/icons/SvgIcon';
+import { usePostFormStore } from '@/stores/post-form';
+import Button from '../Button';
 
 interface BottomSheetPersonnelProps {
   className?: string;
@@ -9,10 +11,45 @@ interface BottomSheetPersonnelProps {
   onClose: () => void;
 }
 
-export default function BottomSheetPersonnel({ className, children, isOpen, onClose }: BottomSheetPersonnelProps) {
+export default function BottomSheetPersonnel({ className, isOpen, onClose }: BottomSheetPersonnelProps) {
+  const [personnel, setPersonnel] = useState(1);
+  const [isComplete, setIsComplete] = useState();
+  const { formData, updateFormData } = usePostFormStore();
+
+  const onClickButton = () => {
+    updateFormData('personnel', `${personnel}인`);
+    onClose();
+  };
   return (
-    <BottomSheet className={className} isOpen={isOpen} onClose={onClose}>
-      {children}
+    <BottomSheet height={'224px'} className={className} isOpen={isOpen} onClose={onClose}>
+      <div className='flex flex-col items-center w-full rounded-t-xl bg-white border-t border-[#d9d9d9]'>
+        <div className='py-4'>
+          <p className='text-xl font-bold text-[#1b1b1b]'>인원을 선택해주세요</p>
+        </div>
+
+        <div className='flex items-center justify-center gap-8 py-6'>
+          <div
+            onClick={() => {
+              if (personnel > 1) setPersonnel(personnel - 1);
+            }}
+            className='cursor-pointer'
+          >
+            <SubtractIcon />
+          </div>
+          <p className='text-3xl font-bold text-[#1b1b1b]'>{personnel}</p>
+          <div
+            onClick={() => {
+              setPersonnel(personnel + 1);
+            }}
+            className='cursor-pointer'
+          >
+            <AddIcon />
+          </div>
+        </div>
+      </div>
+      <Button isComplete={true} onClick={onClickButton}>
+        완료
+      </Button>
     </BottomSheet>
   );
 }
