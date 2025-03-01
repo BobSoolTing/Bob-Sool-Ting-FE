d9c7a3a75e7f444a11bbe6eb76b0e6fe4002f49e
LimJaeHyun <guswogus81@gmail.com>
2025-03-01 23:17:37 +0900

    📝 Update : react-calendar 커스텀 css 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,23 @@ import '@/styles/globals.css';
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import { ReactNode } from 'react';
+import '@/styles/calendar.css';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactNode) => ReactNode;
-}
+};
 
-export default function App({ Component, pageProps }: AppProps & {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
   Component: NextPageWithLayout;
 }) {
   const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
 
   return (
     <div className='w-full h-screen flex justify-center items-center bg-white'>
-      <div className='w-[412px] h-full'>
-        {getLayout(<Component {...pageProps} />)}
-      </div>
+      <div className='w-[412px] h-full'>{getLayout(<Component {...pageProps} />)}</div>
     </div>
   );
 }
