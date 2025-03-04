122883d77f2be9438b48943929920beb4cbf79bc
dlgkdud4759 <106677130+dlgkdud4759@users.noreply.github.com>
2025-03-04 20:59:54 +0900

    📝 Update : 복사 기능 추가

--- src/pages/match/index.tsx
+++ src/pages/match/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
 import { MatchedIcon, CopyIcon } from '@/assets/icons/SvgIcon';
 import ProfileItem from '@/components/shared/ProfileItem';
@@ -6,6 +7,8 @@ import BottomBar from '@/components/shared/bst/BottomBar';
 import Button from '@/components/shared/Button';
 
 export default function MatchedPage() {
+  const router = useRouter();
+  const [copySuccess, setCopySuccess] = useState(false);
   const details = [
     { label: '모집글', value: '밥약 하실 후배님을 찾습니다.' },
     { label: '장소', value: '인천광역시 연수구 송도동' },
@@ -13,6 +16,20 @@ export default function MatchedPage() {
     { label: '인원', value: '2인' },
     { label: '연락처', value: '010 - 0000 - 0000' },
   ];
+
+  const handleCopy = (text: string) => {
+    const cleanedText = text.replace(/\s/g, '');
+
+    navigator.clipboard.writeText(cleanedText).then(
+      () => {
+        setCopySuccess(true);
+        setTimeout(() => setCopySuccess(false), 3000);
+      },
+      (err) => {
+        console.error('복사 실패:', err);
+      }
+    );
+  };
 
   return (
     <div className='flex flex-col min-h-screen'>
@@ -33,9 +50,14 @@ export default function MatchedPage() {
           <div key={index} className={`flex px-2 py-4 ${index !== details.length - 1 ? 'border-b border-[#eee]' : ''}`}>
             <p className='h-4 text-base text-[#767676] flex items-center'>{item.label}</p>
             {item.label === '연락처' ? (
-              <div className='flex ml-auto items-center'>
+              <div className='flex ml-auto items-center cursor-pointer' onClick={() => handleCopy(item.value)}>
                 <p className='h-4 text-base text-[#2768ff] flex items-center mr-1'>{item.value}</p>
-                <CopyIcon />
+                <CopyIcon className='cursor-pointer' />
+                {copySuccess && (
+                  <div className='fixed top-[750px] left-1/2 transform -translate-x-1/2 bg-[#2768ff] text-white px-4 py-2 rounded-md text-lg font-semibold shadow-lg'>
+                    복사됨!
+                  </div>
+                )}
               </div>
             ) : (
               <p className='h-4 text-base text-[#1b1b1b] ml-auto flex items-center'>{item.value}</p>
@@ -47,7 +69,9 @@ export default function MatchedPage() {
         <p className='w-[328px] text-base font-medium text-center text-[#999]'>다른 참여자의 프로필 정보가 궁금하신가요?</p>
       </div>
       <div className='pt-16'>
-        <Button>확인</Button>
+        <Button isComplete={true} onClick={() => router.back()}>
+          확인
+        </Button>
       </div>
     </div>
   );
