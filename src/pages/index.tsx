9ae10c7fab713b9070855f8ab40d940c537042a6
LimJaeHyun <guswogus81@gmail.com>
2025-03-11 13:35:08 +0900

    📝 Update : 메인페이지 카카오 로그인 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image';
 import kakaoLogin from '@/assets/images/kakaoLogin.png';
-import { useRouter } from 'next/router';
 
 export default function Home() {
-  const router = useRouter();
+  const rest_api_key = process.env.NEXT_PUBLIC_KAKAO_CLIENT_KEY;
+  const redirect_url = 'http://localhost:3000/login';
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${rest_api_key}&redirect_uri=${redirect_url}&response_type=code`;
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
 
   return (
     <div className='w-[412px] h-[100vh] bg-white'>
@@ -14,7 +18,7 @@ export default function Home() {
           alt='카카오 로그인 버튼'
           className='w-full max-w-[280px] cursor-pointer'
           onClick={() => {
-            router.push('/login');
+            handleLogin();
           }}
         />
       </div>
