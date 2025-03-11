5815888f7acc8d79569e48b4233beec58aa4fe3b
LimJaeHyun <guswogus81@gmail.com>
2025-03-11 23:44:10 +0900

    📝 Update : 로그인 API 연결

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 export default function LoginPage() {
+  const router = useRouter();
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get('code');
-    console.log(code);
     const url = 'http://bobsoolting-server-env.eba-bkceqrym.ap-northeast-2.elasticbeanstalk.com/api/member/auth/kakao';
     const fetchLogin = async () => {
       try {
         const res = await axios.post(url, {
           code: code,
         });
-        console.log('하하');
-        const data = await res.data;
-        console.log(data);
+        if (res.status === 200) {
+          const data = await res.data;
+          localStorage.setItem('accessToken', data.access_token);
+          localStorage.setItem('refreshToken', data.refresh_token);
+          router.push('/register');
+        }
       } catch (err) {
-        console.error('로그인 과정 중 에러 발생:', err);
+        alert(`로그인 과정 중 에러 발생 : ${err}`);
       }
     };
     fetchLogin();
